23820540d86f35d9cb9f9fbbcf5e9c37ea86575b
aladaabdul <aladarahman18@gmail.com>
2024-10-17 22:47:01 +0100

    modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -29,8 +29,19 @@ const swaggerDocument = yaml.load(
     fs.readFileSync(path.join(__dirname, 'swagger.yaml'), 'utf-8')
 );
 
-app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerDocument!, { customCssUrl: CSS_URL }));
+// app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerDocument!, { customCssUrl: CSS_URL }));
 
+
+// Route-Handler to visit our docs
+app.use(
+    '/api-docs',
+    SwaggerUI.serve,
+    SwaggerUI.setup(swaggerDocument!, {
+      customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      customCssUrl: CSS_URL,
+    }),
+  );
 
 // User routes
 app.use('/api/user', userRouter);
