2998ef7748047e31ebd0a03253f5194d9be4161a
aladaabdul <aladarahman18@gmail.com>
2024-10-17 22:38:07 +0100

    vercel

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json())
 
 // CDN CSS
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 
 
 // const swaggerDocument = yaml.load(fs.readFileSync("./src/swagger.yaml", 'utf-8'));
